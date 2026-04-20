b7164e912601deeb727f7f3e5c6ddda00a300e91
Adrian Ortiz Jr <adrianortizjr@icloud.com>
2026-04-19 17:45:36 -0700

    Improved file management

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,7 +1,12 @@
 import LoginForm from "./LoginForm.jsx";
+import { Box } from "@mui/material";
 
 function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Box>
+      <LoginForm />
+    <Box/>
+    );
 }
 
 export default LoginPage;
