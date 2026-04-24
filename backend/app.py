bda9f512a7e4e8f36cc6f66dccf8ba64a9ddfaea
Adrian Ortiz Jr <adrianortizjr@icloud.com>
2026-04-23 20:25:30 -0700

    Added the basic admin panel

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.theme import Bootstrap4Theme
+from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Admin
 app.config["FLASK_ADMIN_SWATCH"] = "cerulean"
 
-admin = Admin(app, name="microblog", theme=Bootstrap4Theme(swatch="cerulean"))
+admin = Admin(app, name="Chess", theme=Bootstrap4Theme(swatch="cerulean"))
 
 # Database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -18,6 +19,10 @@ class UserCreditials(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, unique=False, nullable=False)
+
+
+# Admin Model Views
+admin.add_view(ModelView(UserCreditials, db.session))
 
 
 if __name__ == "__main__":
