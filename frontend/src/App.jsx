dc5ea2fd61faec037d62480bdeeac3c7e74546a3
Adrian Ortiz Jr <adrianortizjr@icloud.com>
2026-04-24 22:31:25 -0700

    Added import to navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import "./App.css";
 import LoginPage from "./components/LoginPage.jsx";
