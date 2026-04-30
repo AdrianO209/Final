ecea714a1549b91a53cd0a92ac62359a04a77712
Adrian Ortiz Jr <adrianortizjr@icloud.com>
2026-04-30 12:30:28 -0700

    Added some tabs for the dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,12 +1,45 @@
 import { useState } from "react";
-import { Box, Typography } from "@mui/material";
-import { GiChessQueen } from "react-icons/gi";
+import { Box, Typography, Tabs, Tab } from "@mui/material";
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
 
 function Dashboard() {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setActiveTabIndex(newValue);
+  };
+  const sharedTabStyles = {
+    fontSize: "1.1rem",
+    fontWeight: "bold",
+    padding: "20px 30px",
+  };
   return (
-    <Box>
-      <Typography variant="h1">Testing Dashboard jump</Typography>
-    </Box>
+    <>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Tabs value={activeTabIndex} onChange={handleChange}>
+          <Tab label="Create Game!" sx={sharedTabStyles} />
+          <Tab label="Join a Game!" sx={sharedTabStyles} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={activeTabIndex} index={0}>
+        <Typography>Testing</Typography>
+      </CustomTabPanel>
+    </>
   );
 }
 
